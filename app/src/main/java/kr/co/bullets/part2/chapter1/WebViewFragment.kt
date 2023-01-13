97497535b6110bd5ef9dec72261b3d1e9382f69b
Bullets-jyr <jyr@bullets.co.kr>
2023-01-13 10:19:57 +0900

    02. WebView 사용해보기

---
+++ app/src/main/java/kr/co/bullets/part2/chapter1/WebViewFragment.kt
@@ -0,0 +1,29 @@
+package kr.co.bullets.part2.chapter1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import kr.co.bullets.part2.chapter1.databinding.FragmentWebViewBinding
+
+class WebViewFragment : Fragment() {
+    private lateinit var binding: FragmentWebViewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWebViewBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://google.com")
+    }
+}
